08c03ab78dc35c178298b5fe396da9a002f65767
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-01-31 11:41:54 +0200

    Initial commit

---
+++ ex1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first php page</title>
+</head>
+<body>
+    <h1>My simple php script</h1>
+    <?php
+    //opening tag
+    echo "hello world";
+    echo "this is a nie line";
+    echo "i need to use back slash to escape spe char\".
+    to close";
+    echo "hello world this is 'good' ";
+    ?>
+    
+</body>
+</html>
