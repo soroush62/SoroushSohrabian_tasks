1a0bbf2c9c696d42f74e3bbf5867197c49569cd5
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-02-01 09:49:41 +0200

    make some php files

---
+++ layout/ex1.php
@@ -0,0 +1,20 @@
+  <?php include "header.php" ?>
+    <h1>3.1 Write a simple PHP script to print your information (Name and your groupid)</h1>
+    <?php
+    echo "Soroush Sohrabian <br>";
+    echo "bbcap22_6";
+
+    //opening tag
+    echo "hello world";
+    echo "this is a nie line";
+    echo "i need to use back slash to escape spe char\".
+    to close";
+    echo "hello world this is 'good' ";
+    ?>
+    <h1>3.2.Write PHP code to display the following message.</h1>
+    <?php
+     echo "Hello world! My name is \"Soroush\"";
+    ?>
+    <h1>3.3.Write the PHP code in to display the current date.</h1>
+    <?php echo date("d.m.Y") ?>
+    <?php include "footer.php" ?>
---
+++ layout/index.php
@@ -0,0 +1,15 @@
+<?php include "header.php" ?>
+
+<h1>some ciontent</h1>
+<p>Mark Otto announced Bootstrap 4 on October 29, 2014.
+        The first alpha version of Bootstrap 4 was released on August 19, 2015.[10]
+         The first beta version was released on 10 August 2017.[11] 
+         Mark suspended work on Bootstrap 3 on September 6, 2016, to free up 
+      time to work on Bootstrap 4.
+         Bootstrap 4 was finalized on January 18, 2018</p>
+         <p>Mark Otto announced Bootstrap 4 on October 29, 2014.
+        The first alpha version of Bootstrap 4 was released on August 19, 2015.[10]
+         The first beta version was released on 10 August 2017.[11] 
+        </p>
+
+<?php include "footer.php" ?>
