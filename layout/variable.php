d6ed945662bf8454f09046a6778b454b2548af90
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-02-06 17:43:20 +0200

    defining some variables

---
+++ layout/variable.php
@@ -0,0 +1,12 @@
+<?php
+$name = "Soroush";
+$number = 5;
+$_age = 18;
+// $fist name="Pekka"; we can not use spaces
+// $last-name="KC";  we cane not use - just _
+$last_name="KC";
+$sub = "php";
+$Sub = "Database";
+
+echo $name;
+?>
