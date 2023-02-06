9fdd1b8ee779bbd4c9dc8edebc415c35343c6e9f
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-02-06 18:29:52 +0200

    working with variables

--- layout/variable.php
+++ layout/variable.php
@@ -10,3 +10,34 @@ $Sub = "Database";
 
 echo $name;
 ?>
+<?php
+//Global scope
+//$a = 10;
+
+//function test(){
+   // echo $a; //global variable is not accessable in function
+
+//Global 
+//}
+// test();
+?>
+<?php
+//Local scope
+function test1(){
+    $a = 20;
+    echo "<br>". $a; 
+}
+test1();
+echo "<br>" . $a;
+?>
+
+<?php
+$name="Pekka";
+//Local scope
+function test2(){
+    global $name;
+    $b = 20;
+    echo "<br>". $name; 
+}
+test2();
+?>
