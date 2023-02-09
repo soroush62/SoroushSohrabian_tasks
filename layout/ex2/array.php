9e555e618b1322a87280e5c3f02e11b31e5493d9
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-02-09 10:27:53 +0200

    first ex

--- layout/ex2/array.php
+++ layout/ex2/array.php
@@ -5,13 +5,17 @@
       text-align: center !important;
     }
   </style>
-
-
-
-
-
-
-
+  
+<h3>1. Write a php script to display courses as list. Use <li></h3>
+<ul>
+  <li>PHP</li>
+  <li>HTML</li>
+  <li>JavaScript</li>
+  <li>CMS</li>
+  <li>Project</li>
+</ul>
+<?php
+?>
 
 
 
