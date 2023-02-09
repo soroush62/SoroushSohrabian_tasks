9ca9788fc9d205b069a6914c33ebf4db07920227
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-02-09 11:21:07 +0200

    sorting

--- layout/ex2/array.php
+++ layout/ex2/array.php
@@ -8,25 +8,74 @@
   <h3>1. Write a php script to display courses as list. Use <li></h3>
 <?php
 $courses=array("PHP", "HTML", "JavaScript", "CMS", "Project");
+$num = count(($courses));
 echo "<ul>";
-echo "<li>";
-echo $courses[0];
-echo "</li>\n";
-echo "<li>";
-echo $courses[1];
-echo "</li>\n";
-echo "<li>";
-echo $courses[2];
-echo "</li>\n";
-echo "<li>";
-echo $courses[3];
-echo "</li>\n";
-echo "<li>";
-echo $courses[4];
-echo "</li>\n";
-echo "</ul>";
+for ($i = 0; $i < $num; $i++)
+    echo "<li>" . $courses[$i] . "</li>";
+    echo "</ul>";
 ?>
-
+  <h3>1. The unset() function is used to remove element from the array. The var_dump() function is used to dump information about a variable.  array_values() is an inbuilt function that returns all the values of an array and not the keys.
+</h3>
+<h4>Delete an element from the array below:</h4>
+<?php
+$courses1=array("PHP", "HTML", "JavaScript", "CMS", "Project");
+unset($courses1[0]);
+$num = count(($courses1));
+echo "<ul>";
+for ($i = 0; $i < $num; $i++)
+    echo "<li>" . $courses1[$i+1] . "</li>"; 
+    echo "</ul>";  
+?>
+<h4>Sort the following array :</h4>
+<?php
+$courses3=array("PHP", "HTML", "JavaScript", "CMS", "Project");
+sort($courses3);
+$num = count(($courses3));
+echo "<ul>";
+for ($i = 0; $i < $num; $i++)
+    echo "<li>" . $courses3[$i] . "</li>"; 
+    echo "</ul>";  
+?>
+<h4> a.ascending order sort by value:</h4>
+<?php
+$courses3=array("PHP", "HTML", "JavaScript", "CMS", "Project");
+asort($courses3);
+$num = count(($courses3));
+echo "<ul>";
+for ($i = 0; $i < $num; $i++)
+    echo "<li>" . $courses3[$i] . "</li>"; 
+    echo "</ul>";  
+?>
+<h4>b.ascending order sort by Key</h4>
+<?php
+$courses3=array("PHP", "HTML", "JavaScript", "CMS", "Project");
+ksort($courses3);
+$num = count(($courses3));
+echo "<ul>";
+for ($i = 0; $i < $num; $i++)
+    echo "<li>" . $courses3[$i] . "</li>"; 
+    echo "</ul>";  
+?>
+<h4>c.descending order sort by Value</h4>
+<?php
+$courses3=array("PHP", "HTML", "JavaScript", "CMS", "Project");
+arsort($courses3);
+$num = count(($courses3));
+echo "<ul>";
+for ($i = 0; $i < $num; $i++)
+    echo "<li>" . $courses3[$i] . "</li>"; 
+    echo "</ul>";  
+?>
+<h4>d.descending order sort by Key</h4>
+<?php
+$courses3=array("PHP", "HTML", "JavaScript", "CMS", "Project");
+krsort($courses3);
+$num = count(($courses3));
+echo "<ul>";
+for ($i = 0; $i < $num; $i++)
+    echo "<li>" . $courses3[$i] . "</li>"; 
+    echo "</ul>";  
+?>
 
 
 
