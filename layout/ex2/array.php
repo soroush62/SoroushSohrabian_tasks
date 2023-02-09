acaa906047ff6cb0b43c2f427499b52e7dc2630f
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-02-09 10:47:01 +0200

    first ex is done

--- layout/ex2/array.php
+++ layout/ex2/array.php
@@ -5,17 +5,29 @@
       text-align: center !important;
     }
   </style>
-  
-<h3>1. Write a php script to display courses as list. Use <li></h3>
-<ul>
-  <li>PHP</li>
-  <li>HTML</li>
-  <li>JavaScript</li>
-  <li>CMS</li>
-  <li>Project</li>
-</ul>
+  <h3>1. Write a php script to display courses as list. Use <li></h3>
 <?php
+$courses=array("PHP", "HTML", "JavaScript", "CMS", "Project");
+echo "<ul>";
+echo "<li>";
+echo $courses[0];
+echo "</li>\n";
+echo "<li>";
+echo $courses[1];
+echo "</li>\n";
+echo "<li>";
+echo $courses[2];
+echo "</li>\n";
+echo "<li>";
+echo $courses[3];
+echo "</li>\n";
+echo "<li>";
+echo $courses[4];
+echo "</li>\n";
+echo "</ul>";
 ?>
+
+
 
 
 
