7b2e913430f02007e91240bcfe9c36eda315f109
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-02-09 10:12:25 +0200

    is done

--- layout/ex2/arrays.php
+++ layout/ex2/arrays.php
@@ -66,4 +66,19 @@ for ($i=0; $i<$len; $i++){
 }
 
 ?>
+<?php
 
+?>
+//form
+<form action="" method="get">
+    <input type="number" placeholder="Enter your first number" name="num1">
+    <input type="number" placeholder="Enter your second number" name="num2">
+    <select name="operator">
+    <option value="add">Add</option>
+    <option value="sub">Subtract</option>
+    <option value="mul">Multiply</option>
+    <option value="div">Divide</option>
+    </select>
+    <input type="submit" name="cal" value="calculate">
+
+</form>
--- layout/ex2/calculator.php
+++ layout/ex2/calculator.php
@@ -1,9 +1,14 @@
 <?php include "header.php";?>
-
+<style>
+    body{
+      color: white !important;
+      text-align: center !important;
+    }
+  </style>
 
 //form
 <form action="" method="get">
-    <input type="number" placeholder="Enter your first number" name="num1" required>
+    <input type="number" placeholder="Enter your first number" name="num1" required >
     <input type="number" placeholder="Enter your second number" name="num2" required>
     <select name="operator">
     <option value="add">Add</option>
@@ -13,4 +18,30 @@
     </select>
     <input type="submit" name="cal" value="calculate">
 </form>
+<?php
+if(isset($_GET["cal"])){
+    $num1 = $_GET["num1"];
+    $num2 = $_GET["num2"];
+    $operator = $_GET["operator"];
+    switch($operator){
+        case "add":
+            $result = $num1 + $num2;
+        break;
+        case "sub":
+            $result = $num1 - $num2;
+        break;
+        case "mul":
+            $result = $num1 * $num2;
+        break;
+        case "div":
+            $result = $num1 / $num2;
+        break;
+        default:
+            $result = "Err7ror : you have not selected the correct operator";
+    }
+}
+if (isset($result)){
+    echo "<h2> Result: $result</h2>";
+}
+?>
 <?php include "footer.php" ?>
